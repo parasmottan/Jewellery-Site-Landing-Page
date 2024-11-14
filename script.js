6874a74d74173a200c2e35aadf8404464361257b
codexcoder <parasmottan9316@gmail.com>
2024-11-14 13:22:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".next-arrow",
+    prevEl: ".prev-arrow",
+  },
+ 
+  mousewheel: true,
+  keyboard: true,
+});
